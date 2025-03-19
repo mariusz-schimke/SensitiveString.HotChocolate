46f90212391eef6d4eac32a1292df5777ed407ad
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-03-19 22:13:19 +0100

    Cleanup

--- src/TextPrivacy.SensitiveString.HotChocolate/SensitiveStringCursorKeySerializer.cs
+++ src/TextPrivacy.SensitiveString.HotChocolate/SensitiveStringCursorKeySerializer.cs
@@ -1,6 +1,6 @@
 namespace TextPrivacy.SensitiveString.HotChocolate;
 
-public class SensitiveStringCursorKeySerializer : SensitiveStringAbstractCursorKeySerializer
+public sealed class SensitiveStringCursorKeySerializer : SensitiveStringAbstractCursorKeySerializer
 {
     public override bool IsSupported(Type type) => type == typeof(SensitiveString);
     protected override SensitiveString AsSensitiveString(string formattedKey) => formattedKey.AsSensitive();
